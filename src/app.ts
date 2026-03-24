699531885c4c9f18f83fb13ef43ae854eb3efeaa
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2026-03-24 14:18:08 -0300

    feat: add AppError and errorHandling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
+import { errorHandling } from './middlewares/errorHandling';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(errorHandling);
 
 export { app };
