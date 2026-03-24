c8bad3504ff1bf0dce09456edb0aca7c0afd907b
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2026-03-24 16:02:24 -0300

    feat(users): add users routes and controller (#3)

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
 import { errorHandling } from './middlewares/errorHandling';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(errorHandling);
+
+app.use(routes);
 
 export { app };
